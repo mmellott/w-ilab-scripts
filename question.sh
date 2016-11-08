913681454610d619dea306f69ba08053fcec8d1d
mmellott <matthew.mellott@yahoo.com>
2016-11-07 23:04:12 -0700

    Some refinements for the question

--- question.sh
+++ question.sh
@@ -10,17 +10,18 @@ function time_tx
     grep ch_time_tx: | cut -d' ' -f7
 }
 
-t=$(mktemp)
-ethtool -S wlan0 > "$t"
-before_total=$(cat "$t" | time_total)
-before_tx=$(cat "$t" | time_tx)
+before=$(mktemp)
+after=$(mktemp)
 
+ethtool -S wlan0 > "$before"
 dd if=/dev/zero count=1000 | nc "$1" 5000
+ethtool -S wlan0 > "$after"
 
-t=$(mktemp)
-ethtool -S wlan0 > "$t"
-after_total=$(cat "$t" | time_total)
-after_tx=$(cat "$t" | time_tx)
+before_total=$(cat "$before" | time_total)
+before_tx=$(cat "$before" | time_tx)
+after_total=$(cat "$after" | time_total)
+after_tx=$(cat "$after" | time_tx)
 
+echo "($after_tx - $before_tx) / ($after_total - $after_tx)"
 bc -l <<< "($after_tx - $before_tx) / ($after_total - $after_tx)"
 
